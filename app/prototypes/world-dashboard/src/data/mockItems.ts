97ad3338c1d8cfe5e6a6e904634a134929c53d91
Owen Wang <wangowen13@gmail.com>
2026-01-12 10:57:26 +0000

    feat: add world map to spark

---
+++ app/prototypes/world-dashboard/src/data/mockItems.ts
@@ -0,0 +1,6 @@
+export type Item = { id: string; name: string; countries: string[] };
+
+export const mockItems: Item[] = [
+  { id: "HSHEBLAN03", name: "Blanket cotton", countries: ["AFG", "ALB", "DZA"] },
+  { id: "HSHEETAR01", name: "Tarpaulin", countries: ["KEN", "UGA", "NGA"] },
+];
